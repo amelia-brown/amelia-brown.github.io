bac8b20efe77bd5526f5ca19bd39ae4638e3606e
Amelia Brown <amelia.brown389@gmail.com>
2017-06-28 00:47:17 -0700

    [chore] initial commit set up repo

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer-loader');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'js', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  devtool: '#eval',
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.css',
+      '.sass',
+    ],
+    modules: [
+      'node_modules',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        use: ['json-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]',
+            'postcss-loader',
+            'sass-loader',
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {warnings: false},
+      output: {comments: false},
+      sourcemap: false,
+    })
+  ],
+}
